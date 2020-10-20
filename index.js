8f7421b5b3ff5fef7836b817e385e8adc1eaa93b
Chloe Steffens <caforonda103@yahoo.com>
2020-10-20 06:04:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-let totalBatteries = batteryBatches.reduce((total, amount))
+let totalBatteries = batteryBatches.reduce((total, amount) => total + amount)
